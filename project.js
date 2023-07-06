24b8d2e7ae6ebaaaedbd39067a74ac003e6f8c88
Rituraj Dey <riturajdey01@gmail.com>
2023-07-06 19:44:40 +0530

    Done Step - 4

--- project.js
+++ project.js
@@ -8,6 +8,24 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+
+const SYMBOLS_VALUE = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
+
 const deposit = () => {
     while(true) {
         const depositAmount = prompt("Enter a deposit amount: ")
@@ -70,8 +88,66 @@ const getBet = (balance,lines) => {
     }
 }
 
+
+const Spin = () => {
+    const symbols = [];
+    for(const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for(let i = 0 ; i < count ; i++) {
+            symbols.push(symbol)
+        }
+    }
+
+    const reels = [] ;
+    for (let i = 0; i < COLS; i++)
+    {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for(let j = 0; j < ROWS ; j++)
+        {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+
+        }
+    }
+
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for(let i = 0 ; i < ROWS ; i++)
+    {
+        rows.push([]);
+        for(let j = 0; j < COLS ; j++)
+        {
+            rows[i].push(reels[j][i])
+        }
+    }
+
+    return rows;
+};
+
+const printRows = (rows) => {
+    for(const row of rows)
+    {
+        let rowString = "";
+        for(const [i, symbol] of row.entries()){
+            rowString += symbol
+            if (i != row.length - 1){
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+}
+
+
 let balance = deposit()
-// console.log('Deposited amount is: ',depositAmount);
 const lines = numberOfLines()
-// console.log('Total no of lines are: ',lines);
 const bet = getBet(balance,lines)
+const reels = Spin();
+const rows = transpose(reels);
+printRows(rows);
